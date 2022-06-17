130a5fad0543c492500d6ad754182f769234aa43
mohan <mohansahualbert@gmail.com>
2022-06-17 23:21:58 +0530

    connected file-sending with message

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,6 @@
+// var is_user_connected  = false ; // store if user has successfully created session with socket server or not 
+var set_interval_timeid  ; 
+
 socket.on("calling", (data) => {
     console.log("calling...");
     console.log( data )
@@ -12,35 +15,53 @@ socket.on("redirect", ( data) => {
    
   });
 
+  socket.on("connection-sucess", ( data) => {
+    console.log( "created session succesfully") ; 
+    clearInterval(set_interval_timeid ) ; 
+  });
+
+  
 
   window.addEventListener( "load", (e) => {
     console.log('page is fully loaded');
  
+    set_interval_timeid =    setInterval(make_session_with_socket, 1000  ) ; 
 
     // setTimeout(() => {
         
-    // let data = document.cookie.split(";")
-    let sid =  getCookie("sid")
-    if( !sid) { 
-        console.warn ( "sid not found ")
-    }
-    let cookie_data = { sid };
-    // console.log ( sid) 
-    // console.log ( data) 
-
-    // let temp;
-    // for (let i = 0; i < data.length; i++) {
-    //     temp = data[i].split("=");
-    //     cookie_data[temp[0].trim()] = temp[1];
+    // // let data = document.cookie.split(";")
+    // let sid =  getCookie("sid")
+    // if( !sid) { 
+    //     console.warn ( "sid not found ")
     // }
+    // let cookie_data = { sid };
+    // // console.log ( sid) 
+    // // console.log ( data) 
 
-    console.log(cookie_data);
+    // // let temp;
+    // // for (let i = 0; i < data.length; i++) {
+    // //     temp = data[i].split("=");
+    // //     cookie_data[temp[0].trim()] = temp[1];
+    // // }
+
+    // console.log(cookie_data);
  
 
-    socket.emit("user-connected", cookie_data);
+    // socket.emit("user-connected", cookie_data);
+
+
     // }, 1500);
 
 });
+ 
+function make_session_with_socket ( ){  
+       let sid =  getCookie("sid")
+       if( !sid) { 
+           console.warn ( "sid not found ")
+       }
+       let cookie_data = { sid };  socket.emit("user-connected", cookie_data);
+    
+}
 
 
 // alert("common")
