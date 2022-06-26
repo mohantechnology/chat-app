bf8ce1fa0038532dca8f36a497856dbeaf24ba84
mohan <mohansahualbert@gmail.com>
2022-06-27 00:23:23 +0530

    removed unncessary line from index.js

--- config/database.js
+++ config/database.js
@@ -1,33 +1,29 @@
 // require('dotenv').config();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const fs = require('fs');
 
 const DatabaseSetup = () => {
-    const DB_LINK = process.env.DB_LINK;
+    const { DB_LINK } = process.env;
 
-    let options = {
+    const options = {
         retryWrites: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
-        w: "majority",
+        w: 'majority',
     };
 
-
-
     mongoose.connect(DB_LINK, options)
-        .then(conn => {
-            console.log("Connection to database sucessful");
+        .then((conn) => {
+            console.log('Connection to database sucessful');
         })
-        .catch(err => {
-
+        .catch((err) => {
             // console.log("Failed to Connect with database");
             // console.log(err);
             setTimeout(() => {
-                console.log("Retrying to connect with database.....");
-                DatabaseSetup()
-            }, 3000)
-
-        })
-}
+                console.log('Retrying to connect with database.....');
+                DatabaseSetup();
+            }, 3000);
+        });
+};
 module.exports = DatabaseSetup;
--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-"use strict"
 const app = require('./config/app');
 // const constant = require('./Utils/constant.js');
-const fs = require('fs');
 require('./config/database')();
 require('./config/socket')();
 
- 
-
-var port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000;
 
 // var cors_opt = {
 //   origin:"https://php-file-api.000webhostapp.com/transfer_file.php"
 // }
- 
 
-app.all("/*", (req, res) => {
-  res.status(404).send({ "status": "error", message: "page not found from 1" })
-})
-
-
-
-app.listen(port, () => {
-  console.log("listening at " + port);
+app.all('/*', (req, res) => {
+    res.status(404).send({ status: 'error', message: 'page not found from 1' });
 });
 
-
+app.listen(port, () => {
+    //  eslint-disable-next-line no-console
+    console.log(`listening at ${port}`);
+});
