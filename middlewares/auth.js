a71af04a99bb8f0f24631ac191f005a3af578df5
mohan <mohansahualbert@gmail.com>
2022-07-10 05:05:09 +0530

    linted middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,21 +3,17 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
 
-    const token = req.cookies.sid || req.cookies.lid || req.headers["x-access-token"];
-    // console.log( token)
-    if (!token) {
-        return res.status(400).json({ message: "Please Login" })
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.user = decoded;
-    } catch (err) {
+  const token = req.cookies.sid || req.cookies.lid || req.headers["x-access-token"];
+  if (!token) {
+    return res.status(400).json({ message: "Please Login" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = decoded;
+  } catch (err) {
 
-        return res.status(401).json({ message: "Authorization Failed. Please Loging Again" })
-    }
-    return next();
+    return res.status(401).json({ message: "Authorization Failed. Please Loging Again" });
+  }
+  return next();
 
 };
-
-
-
--- middlewares/catchError.js
+++ middlewares/catchError.js
@@ -1,108 +1,65 @@
-
- 
- 
   
 const handleDuplicateKeyError = (err, res) => {
-    // console.log("ersdsdsr"); 
-    // console.log(err); 
-    // console.log(err); 
-    try { 
-        const fieldNameList =  err && err.keyValue && Object.keys(err.keyValue) || [];
-        // const error = `An account with that ${field} already exists.`;
+  try { 
+    const fieldNameList =  err && err.keyValue && Object.keys(err.keyValue) || [];
+    // const error = `An account with that ${field} already exists.`;
     
-        const error = `An account with given values ${fieldNameList.join(",")}  already exists.`;
-        res.status(err.statusCode || 409 ).json({
-            message : error
-        })
-    }
-    catch (err ){
-        console.log(err)
-        res.status(500).json({ message : "Something went wrong at 'handleDuplicateKeyError' "  })
-    }
+    const error = `An account with given values ${fieldNameList.join(",")}  already exists.`;
+    res.status(err.statusCode || 409 ).json({
+      message : error
+    });
+  }
+  catch (err ){
+    res.status(500).json({ message : "Something went wrong at 'handleDuplicateKeyError' "  });
+  }
  
-}
- const handleValidationError = (err, res) => {
-    let error = Object.values(err.errors).map(el => el.path);
+};
+ 
+const handle_mongoose_validation_error = (err, res) => {
+  try{ 
+        
+    let errorList = []; 
+    let error = Object.values(err.errors).map((el )=> {
+      errorList.push( { fieldName:el.path , message:el.message });
+      return el.path;
+    });
     if(error.length > 1) {
-        error = error.join(',')
+      error = error.join(',');
     }
     error = "Enter valid values for " + error;
     res.status(err.statusCode || 400).json({
-        message : error
-    })
-}
-
-const handle_mongoose_validation_error = (err, res) => {
-    try{ 
-        
-        // console.log( "START of handle_mongoose_validation_error")
-        let errorList = []; 
-        let error = Object.values(err.errors).map((el )=> {
-            errorList.push( { fieldName:el.path , message:el.message })
-            return el.path;
-        });
-        if(error.length > 1) {
-            error = error.join(',')
-        }
-        error = "Enter valid values for " + error;
-        res.status(err.statusCode || 400).json({
-            message : error ||  "Validation Error", 
-            errorList , 
-        })
-        // console.log( "End of handle_mongoose_validation_error")
-    }
-    catch ( err) { 
-        console.log( err)
-        // res.status(500).json({
-        //     message :err,  
-        // })
-    }
+      message : error ||  "Validation Error", 
+      errorList , 
+    });
+  }
+  catch ( err) { 
+    console.error( err) ; 
+    // res.status(500).json({
+    //     message :err,  
+    // })
+  }
     
-}
-
-
-
+};
  
- module.exports  = (func) => {
-    //  console.log( "inside catch error func")
-    return (req, res, next) => {
+module.exports  = (func) => {
+  return (req, res, next) => {
 
-        // console.log( req) ; 
-        //   console.log( "Calling with req, res, next")
-        // func(req, res, next).catch(next);
-        func(req, res, next).catch((err)=>{
-            // console.log( "err  --> "); 
-            // console.log( err) ; 
-       
-            
-            // console.log( "err.name  --> "); 
-            // console.log( err.name) ; 
-            
-            // console.log( "err.code  --> "); 
-            // console.log( err.code) ; 
+    // func(req, res, next).catch(next);
+    func(req, res, next).catch((err)=>{
 
-            //       console.log( "err.statusCode  --> "); 
-            // console.log( err.statusCode) ; 
-
-            try{ 
-                if(err.name === 'ValidationError'    ) { return   handle_mongoose_validation_error(err, res);}
-                if(err.name === 'MongoServerError') return err = handleDuplicateKeyError(err, res);
-                else if(err.code && err.code == 11000) return err = handleDuplicateKeyError(err, res);
-                else res.status(err.statusCode||500).json({
-                    message : err.message
-                })
-            }
-            catch ( err) { 
-                console.log( err)
-                // res.status(500).json({
-                //     message :err,  
-                // })
-            }
-           
-
-
-            // res.send( {error: error})
-            // next()
+      try{ 
+        if(err.name === 'ValidationError'    ) { return   handle_mongoose_validation_error(err, res);}
+        if(err.name === 'MongoServerError') return err = handleDuplicateKeyError(err, res);
+        else if(err.code && err.code == 11000) return err = handleDuplicateKeyError(err, res);
+        else res.status(err.statusCode||500).json({
+          message : err.message
         });
-    };
+      }
+      catch ( err) { 
+        console.error( err);
+       
+      }
+    
+    });
+  };
 };
