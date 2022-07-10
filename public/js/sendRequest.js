021fb76b5492fa6e8accc6452ec3c050f5efe7ab
mohan <mohansahualbert@gmail.com>
2022-07-11 02:22:20 +0530

    removed unncessary line

--- public/js/sendRequest.js
+++ public/js/sendRequest.js
@@ -7,69 +7,30 @@ var sendRequest  = {
 
     return new Promise((resolve, reject) => {
     
-      // var formData = new FormData();
-      // formData.append("thefile", file);
-    
       var xhttp = new XMLHttpRequest();
-    
-      /*
-    
-    Access-Control-Allow-Origin:  http://127.0.0.1:3000
-    Access-Control-Allow-Methods: POST
-    Access-Control-Allow-Headers: Content-Type, Authorization
-    
-                /*/
             
       xhttp.onreadystatechange = function () {
     
-        // console.log( "ready state is 4 response is  " ,this.status )
-        // console.log( "data is ",this.response)
-
         if (this.readyState === 4  ) {
-          // console.log( this.status); 
-          // console.log( "ready state is 4 response is  " ,this.status )
+ 
           if(this.status >= 200 && this.status<300){ 
                         
             resolve( this.response);
-            // console.log( "data is ",this.response)
-            // console.log(  this )
+           
           }
           else{ 
             reject( this.response);
           }
     
         }
-        //  console.log( this.response); 
+        
       };
-          
-      xhttp.onprogress = function (event) {
-        // console.log( "proses**lkj")
-            
-      };
-            
-      //   xhttp.onreadystatechange =( e) =>{
-      //          console.log( "proseslkj")
-      //     console.log( e.loaded )
-      //     console.log( e.total )
-      //     console.log( "ready state is 4 response is  " ,this.status )
-      //     console.log( "data is ",this.response)
-  
-      //   }
 
       xhttp.open("PUT", url, true);
-            
-      // xhttp.setRequestHeader("Access-Control-Allow-Origin" ,"http://127.0.0.1" );
-      // xhttp.setRequestHeader("Access-Control-Allow-Methods" ,"POST" );
-      // xhttp.setRequestHeader("Access-Control-Allow-Headers" ,"Content-Type, Authorization" );
-      // xhttp.setRequestHeader(  'Content-Type ', "application/pdf"  );
-      // xhttp.setRequestHeader(  'Access-Control-Allow-Origin', "*"  );
 
       // xhttp.withCredentials = true; // pass along cookiesdocument.cookie
       xhttp.setRequestHeader('Content-type', file.type);
-            
-      // xhttp.setRequestHeader('Content-type', 'application/pdf');
-      /// aws s3 cp corporateinstructordev/Course/bajcscsaiyxf8v9.png home/bitnami/my-app/src/
-      // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      
       xhttp.send(file);
     }
     
@@ -82,69 +43,30 @@ var sendRequest  = {
 
     return new Promise((resolve, reject) => {
 
-      // var formData = new FormData();
-      // formData.append("thefile", file);
-
       var xhttp = new XMLHttpRequest();
-
-      /*
-
-Access-Control-Allow-Origin:  http://127.0.0.1:3000
-Access-Control-Allow-Methods: POST
-Access-Control-Allow-Headers: Content-Type, Authorization
-
-            /*/
         
       xhttp.onreadystatechange = function () {
 
-        // console.log( "ready state is 4 response is  " ,this.status )
-        // console.log( "data is ",this.response)
-
         if (this.readyState ===4  ) {
-          // console.log( this.status); 
-          // console.log( "ready state is 4 response is  " ,this.status )
+          
           if(this.status >= 200 && this.status<300){ 
                     
             resolve( this.response);
-            // console.log( "data is ",this.response)
-            // console.log(  this )
+         
           }
           else{ 
             reject( this.response);
           }
 
         }
-        //  console.log( this.response); 
+    
       };
-      
-      xhttp.onprogress = function (event) {
-        // console.log( "proses**lkj")
-        
-      };
-        
-      //   xhttp.onreadystatechange =( e) =>{
-      //          console.log( "proseslkj")
-      //     console.log( e.loaded )
-      //     console.log( e.total )
-      //     console.log( "ready state is 4 response is  " ,this.status )
-      //     console.log( "data is ",this.response)
-
-      //   }
 
       xhttp.open("POST", url, true);
-        
-      // xhttp.setRequestHeader("Access-Control-Allow-Origin" ,"http://127.0.0.1" );
-      // xhttp.setRequestHeader("Access-Control-Allow-Methods" ,"POST" );
-      // xhttp.setRequestHeader("Access-Control-Allow-Headers" ,"Content-Type, Authorization" );
-      // xhttp.setRequestHeader(  'Content-Type ', "application/pdf"  );
-      // xhttp.setRequestHeader(  'Access-Control-Allow-Origin', "*"  );
 
       // xhttp.withCredentials = true; // pass along cookiesdocument.cookie
       xhttp.setRequestHeader('Content-type', file.type);
-        
-      // xhttp.setRequestHeader('Content-type', 'application/pdf');
-      /// aws s3 cp corporateinstructordev/Course/bajcscsaiyxf8v9.png home/bitnami/my-app/src/
-      // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     
       xhttp.send(file);
     }
 
@@ -164,32 +86,23 @@ Access-Control-Allow-Headers: Content-Type, Authorization
         
       xhttp.onreadystatechange = function () {
 
-        // console.log( "ready state is 4 response is  " ,this.status )
-        // console.log( "data is ",this.response)
-
         if (this.readyState ===4  ) {
-          // console.log( this.status); 
-          // console.log( "ready state is 4 response is  " ,this.status )
+       
           if(this.status >= 200 && this.status<300){ 
                     
             resolve( this.response);
-            // console.log( "data is ",this.response)
-            // console.log(  this )
+        
           }
           else{ 
             reject( this.response);
           }
 
         }
-        //  console.log( this.response); 
+       
       };
 
       xhttp.open("POST", url, true);
-
-      // xhttp.setRequestHeader('Content-type', file.type);
-        
-      // xhttp.setRequestHeader('Content-type', 'application/pdf');
-      /// aws s3 cp corporateinstructordev/Course/bajcscsaiyxf8v9.png home/bitnami/my-app/src/
+ 
       xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
       xhttp.send(formData);
     }
@@ -202,8 +115,7 @@ Access-Control-Allow-Headers: Content-Type, Authorization
   post: (param, url , content_type, options={}) => {
 
     return new Promise((resolve, reject) => {
-      // createCORSRequest( )
-      // createCORSRequest()
+      
       let defaultOpitons ={
         isSetHeader : true , 
         defaultContentType: "application/x-www-form-urlencoded",
@@ -213,8 +125,7 @@ Access-Control-Allow-Headers: Content-Type, Authorization
       var xhttp = new XMLHttpRequest();
 
       xhttp.onreadystatechange = function () {
-        if (this.readyState ===4  ) {
-          // console.log( this.status); 
+        if (this.readyState ===4  ) { 
           if(this.status >= 200 && this.status<300){ 
                         
             resolve( this.response);
@@ -229,10 +140,6 @@ Access-Control-Allow-Headers: Content-Type, Authorization
       if (defaultOpitons.isSetHeader) {
         xhttp.setRequestHeader("Content-type", content_type || defaultOpitons.defaultContentType );
       }
-
-      // xhttp.setRequestHeader(  'Access-Control-Allow-Origin', "*"  );
-           
-      // xhttp.setRequestHeader("Content-type", "application/json");
             
       xhttp.withCredentials = true; // pass along cookies
       xhttp.send(param);
@@ -246,8 +153,7 @@ Access-Control-Allow-Headers: Content-Type, Authorization
   get_file: ( url) => {
 
     return new Promise((resolve, reject) => {
-      // createCORSRequest( )
-      // createCORSRequest()
+     
       var xhttp = new XMLHttpRequest();
 
       xhttp.onreadystatechange = function () {
@@ -264,15 +170,10 @@ Access-Control-Allow-Headers: Content-Type, Authorization
         }
       };
       xhttp.open("GET", url, true);
-
-      //content-type:"application/octet-stream"
+ 
       xhttp.setRequestHeader("Content-type", "application/octet-stream");
       xhttp.withCredentials = true; // pass along cookies
-      // xhttp.setRequestHeader("Content-type", "image/jpeg");
-      //Content-Disposition: attachment; filename=quot.pdf;
-      // / xhttp.setRequestHeader(  'attachment','attachment' );
-      // xhttp.setRequestHeader(  'Access-Control-Allow-Origin', "*"  );
-      //    xhttp.setRequestHeader(  'Content-Disposition','attachment' );
+    
       xhttp.send();
     }
 
@@ -283,8 +184,7 @@ Access-Control-Allow-Headers: Content-Type, Authorization
   get: ( url) => {
 
     return new Promise((resolve, reject) => {
-      // createCORSRequest( )
-      // createCORSRequest()
+ 
       var xhttp = new XMLHttpRequest();
 
       xhttp.onreadystatechange = function () {
@@ -301,9 +201,9 @@ Access-Control-Allow-Headers: Content-Type, Authorization
         }
       };
       xhttp.open("GET", url, true);
-      // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     
       xhttp.withCredentials = true; // pass along cookies
-      // xhttp.withCredentials
+       
       xhttp.send();
     }
 
