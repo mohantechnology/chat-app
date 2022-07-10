20fdeb6b62328f9edab9c6647eecda542180ce9b
mohan <mohansahualbert@gmail.com>
2022-07-10 05:47:27 +0530

    remove unnessary lines

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ require('./config/socket')();
 
 const port = process.env.PORT || 3000;
 
-
-
 app.all('/*', (req, res) => {
-    res.status(404).send({ status: 'error', message: 'page not found' });
+  res.status(404).send({ status: 'error', message: 'page not found' });
 });
 
 app.listen(port, () => {
-    //  eslint-disable-next-line no-console
-    console.log(`listening at ${port}`);
+  //  eslint-disable-next-line no-console
+  console.log(`listening at ${port}`);
 });
